548e9619d4519baa308850b7afe84b1aaf913765
matecom57 <matecom57@gmail.com>
2025-11-19 05:22:40 -0600

    Test comité

---
+++ lee_file.py
@@ -0,0 +1,20 @@
+import pandas as pd
+ 
+file='PathFinder_Raw_Data/Watermaze_output/31-1.csv'
+
+df = pd.read_csv(file)
+
+key = df.keys()
+
+for i in range(1):
+  k = 3*i
+  nom1 = key[k]
+  nom2 = key[k+1]
+  nom3 = key[k+2]
+  datos = df[[nom1, nom2, nom3]]
+  print(datos)
+  datos.to_csv(nom1+'.csv', index=False)
+
+
+
+
